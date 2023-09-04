3bf7f232dd2e266a63456eaeca5d52f9d206a23f
josueJURE <josue.jure@gmail.com>
2023-09-04 11:18:17 +0100

    feat(CSS): use transform origin on clock's hands

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const hoursHand = document.querySelector("[data-hour-hand ]");
+const minutesHand = document.querySelector("[data-minute-hand]");
+const secondsHand = document.querySelector("[data-second-hand]");
 
+function getTime() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const second = now.getSeconds();
+    const timeInterval = 6;
+    console.log(second)
+
+
+    
+    hoursHand.style.transform = `rotate(${hours}deg)`;
+}
+
+getTime()
